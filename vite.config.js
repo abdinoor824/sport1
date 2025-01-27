2a6e7a2ed9d514df477541178dfcaf73f6443b1b
abdinoor <abdinurkune@gmail.com>
2025-01-27 19:39:09 +0300

    chnges

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 10000, // Use the port from the environment variable or default to 3000
+    host: '0.0.0.0', 
+}});
