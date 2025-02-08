a169ddb4573f607a21395904328e16da0917d00f
abdinoor <abdinurkune@gmail.com>
2025-02-08 21:26:47 +0300

    changed

--- src/components/pages/foters.jsx
+++ src/components/pages/foters.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import "./foters.css"
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
+
+
 const Footer = () => {
   return (
     
@@ -9,6 +11,7 @@ const Footer = () => {
         <div className="social-icons">
           <a href="https://www.facebook.com/profile.php?id=100075192279802" className="social-icon"><i className="fab fa-facebook-f"></i></a>
        <a href="https://my-portfolio-1-ttn1.onrender.com/">visit our website</a>
+       
         </div>
         <p>&copy; 2025 Galaxy 11 football. All rights reserved.</p>
       </div>
