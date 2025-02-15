71dc27e11446d9b26bed576349f86809446484e3
abdinoor <abdinurkune@gmail.com>
2025-02-15 09:02:53 +0300

    changed the website link

--- src/components/pages/foters.jsx
+++ src/components/pages/foters.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <div className="container">
         <div className="social-icons">
           <a href="https://www.facebook.com/profile.php?id=100075192279802" className="social-icon"><i className="fab fa-facebook-f"></i></a>
-       <a href="https://my-portfolio-1-ttn1.onrender.com/">visit our website</a>
+       <a href="https://my-portfolio-1-azure.vercel.app/">visit our website</a>
        
         </div>
         <p>&copy; 2025 Galaxy 11 football. All rights reserved.</p>
